44eb9b1cb2b513aaf5b42a665441c64cc9509326
Rom's <romain.kelifa@gmail.com>
2023-05-28 14:52:20 +0700

    :recycle: refactor from review
    thanks to @taintegral on discord :)

--- src/lib.rs
+++ src/lib.rs
@@ -24,11 +24,11 @@ mod tests {
     use fake::{Fake, Faker};
     use memmap2::Mmap;
     use rkyv::{
-        archived_value,
+        archived_root,
         ser::{serializers::AllocSerializer, Serializer},
     };
 
-    use crate::{ArchivedMeta, Meta};
+    use crate::Meta;
 
     const DIR: &str = env!("CARGO_MANIFEST_DIR");
     const SRC: &str = formatcp!("{}{}{}", DIR, '/', "archive.zip");
@@ -37,7 +37,7 @@ mod tests {
     const NAME: &str = "Jean-Guy";
     const AGE: u8 = 35;
 
-    const SIZE_POS: usize = 4;
+    const SIZE_LEN: usize = 4;
 
     #[allow(unused_variables)]
     fn setup() -> Vec<u8> {
@@ -54,23 +54,12 @@ mod tests {
             age: AGE,
         };
         let mut serializer = AllocSerializer::<1024>::default();
-        let pos = serializer
+        serializer
             .serialize_value(&meta)
             .expect("failed to archive meta");
-        // dbg!(unsafe {
-        //     ::core::slice::from_raw_parts(
-        //         (&meta as *const Meta) as *const u8,
-        //         ::core::mem::size_of::<Meta>(),
-        //     )
-        // }
-        // .len()); // 104
-        // dbg!(&serializer.into_serializer().into_inner().len()); // 56
-        dbg!(&pos);
         let bytes = serializer.into_serializer().into_inner();
         let mut file = File::create(DST).expect("open cust file");
-        let pos = pos as u32;
-        // pos varies depending on content, so let's store it
-        file.write(&pos.to_le_bytes())
+        file.write(&(bytes.len() as u32).to_le_bytes())
             .expect("write cust pos bytes");
         file.write(bytes.as_slice()).expect("write cust meta bytes");
         let written = file.write(blob.as_slice()).expect("write cust zip bytes");
@@ -82,17 +71,16 @@ mod tests {
     fn mmap_partial() {
         let blob = setup();
 
-        const SIZE_META: usize = std::mem::size_of::<ArchivedMeta>();
         let file = File::open(DST).expect("open cust file");
         let mmap = unsafe { Mmap::map(&file).expect("map file in memory") };
-        let pos: [u8; SIZE_POS] = mmap[..SIZE_POS].try_into().expect("read pos");
-        let pos = u32::from_le_bytes(pos) as usize;
-        dbg!(&pos);
-        let archived = unsafe { archived_value::<Meta>(&mmap[..SIZE_META], SIZE_POS + pos) };
+        let header_len: [u8; SIZE_LEN] = mmap[..SIZE_LEN].try_into().expect("read pos");
+        let header_len = u32::from_le_bytes(header_len) as usize;
+        dbg!(&header_len);
+        let archived = unsafe { archived_root::<Meta>(&mmap[SIZE_LEN..SIZE_LEN + header_len]) };
         assert_eq!(archived.name, NAME);
         assert_eq!(archived.age, AGE);
         dbg!(&archived.one, &archived.two, &archived.three);
-        let zip = &mmap[SIZE_POS + pos + SIZE_META..];
+        let zip = &mmap[SIZE_LEN + header_len..];
         dbg!(zip[..].len());
         assert_eq!(zip.len(), blob.len());
         assert_eq!(zip[..], blob[..]);
